8ea11471f1c14cc7e2b4ba38ee81076529c1a0e0
manojyadwad <manojyadwad@gmail.com>
2025-10-08 06:22:20 +0530

    Add files via upload

---
+++ _content/LearnBlazor.Shared/js/common.js
@@ -0,0 +1,30 @@
+window.showToastr = function (type, message)
+{
+    if (type === 'success')
+    {
+        toastr.success(message);
+    }
+
+    if (type === 'error') {
+        toastr.error(message);
+    }
+}
+
+window.ShowSwal = function (type, message) {
+    if (type == 'success') {
+        Swal.fire({
+            title: "Good job!",
+            text: message,
+            icon: "success"
+        });
+    }
+    if (type == 'error') {
+        
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: message,
+            
+        });
+    }
+}
